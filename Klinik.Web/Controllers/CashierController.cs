7e7082c2901080fd3ab9bd2b93c040d9b605fa8c
Mande Muhtar <mande.muhtar155@gmail.com>
2019-04-29 14:14:37 +0700

    Cashier

---
+++ Klinik.Web/Controllers/CashierController.cs
@@ -0,0 +1,21 @@
+﻿using Klinik.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Klinik.Web.Controllers
+{
+    public class CashierController : Controller
+    {
+        // GET: Cachier
+        #region ::Cachier::
+        [CustomAuthorize("VIEW_M_CASHIER")]
+        public ActionResult ListPatien()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
