d2686f26154b6c178d757b85ac5acfe1723b7b44
Mande Muhtar <mande.muhtar155@gmail.com>
2019-05-05 23:48:22 +0700

    Cashier

--- Klinik.Entities/Cashier/CashierModel.cs
+++ Klinik.Entities/Cashier/CashierModel.cs
@@ -5,6 +5,7 @@ namespace Klinik.Entities.Cashier
     public class CashierModel : BaseModel
     {
         public string ItemName { get; set; }
-        public decimal? price { get; set; }
+        public Nullable<int> Price { get; set; }
+        public long PatientID { get; set; }
     }
 }
--- Klinik.Web/Controllers/CashierController.cs
+++ Klinik.Web/Controllers/CashierController.cs
@@ -4,11 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Klinik.Features.Cashier;
+using Klinik.Entities.Cashier;
+using Klinik.Data;
+using Klinik.Data.DataRepository;
 
 namespace Klinik.Web.Controllers
 {
     public class CashierController : Controller
     {
+
+        private IUnitOfWork _unitOfWork;
+        private KlinikDBEntities _context;
+
+        public CashierController(IUnitOfWork unitOfWork, KlinikDBEntities context)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
         // GET: Cachier
         #region ::Cachier::
         [CustomAuthorize("VIEW_M_CASHIER")]
@@ -18,10 +31,20 @@ namespace Klinik.Web.Controllers
         }
 
         [HttpGet]
-        public ActionResult Detail(long? patienid)
+        public ActionResult Detail(long patienid)
         {
-
+            var response = new CashierHandler(_unitOfWork).GetDetail(patienid);
+            ViewBag.Formmedicalid = _unitOfWork.FormMedicalRepository.Get(a => a.PatientID == patienid).Select(x => x.ID).FirstOrDefault();
+            ViewBag.Detail = response.Data;
+            ViewBag.Sum = response.Data.Sum(a => a.Price);
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(long medicalid, FormMedical formMedical)
+        {
+            var response = new CashierHandler(_unitOfWork).update(medicalid, formMedical);
+            return RedirectToAction("ListPatien", "Cashier");
         }
         #endregion
     }
