9514c2642a2a40f820186b98a28f39d778998a9c
Helmy Satria <satria.motorsport@gmail.com>
2019-07-30 06:03:48 +0700

    Initial Implementation of Farmasi

---
+++ Klinik.Features/Farmasi/FarmasiHandler.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Klinik.Common;
+using Klinik.Data;
+using Klinik.Data.DataRepository;
+using Klinik.Entities.Form;
+using Klinik.Entities.Loket;
+using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Klinik.Features.Farmasi
+{
+    public class FarmasiHandler : BaseFeatures
+    {
+        public FarmasiHandler(IUnitOfWork unitOfWork, KlinikDBEntities context = null)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+
+        public FarmasiResponse CreateOrEdit(FarmasiRequest request)
+        {
+            FarmasiResponse response = new FarmasiResponse();
+
+            return response;
+        }
+
+        public static List<Int32> GetSelectedFarmasiItem(long IdQueue)
+        {
+            List<Int32> farmasiItemIds = new List<Int32>();
+            var _getFormMedical = _unitOfWork.RegistrationRepository.GetById(IdQueue);
+            if (_getFormMedical != null)
+            {
+                var qryLabItems = _unitOfWork.FormExamineMedicineRepository.Get(x => x.FormExamine.FormMedicalID == _getFormMedical.FormMedicalID);
+                foreach (var item in qryLabItems)
+                {
+                    farmasiItemIds.Add((int)item.ID);
+                }
+            }
+            return farmasiItemIds;
+        }
+
+        public LoketResponse GetListData(LoketRequest request)
+        {
+            var _laboratoriumId = _unitOfWork.PoliRepository.GetFirstOrDefault(x => x.Name == Constants.NameConstant.Laboratorium);
+            Expression<Func<QueuePoli, bool>> _serachCriteria = x => x.PoliTo == _laboratoriumId.ID;
+
+            List<LoketModel> lists = base.GetbaseLoketData(request, _serachCriteria);
+            int totalRequest = lists.Count();
+            var response = new LoketResponse
+            {
+                Draw = request.Draw,
+                RecordsFiltered = totalRequest,
+                RecordsTotal = totalRequest,
+                Data = lists
+            };
+
+            return response;
+        }
+
+        public FarmasiResponse GetFarmasiForInput(FarmasiRequest request)
+        {
+            List<FormExamineMedicineModel> lists = new List<FormExamineMedicineModel>();
+            dynamic qry = null;
+            var searchPredicate = PredicateBuilder.New<FormExamineMedicine>(true);
+
+            searchPredicate = searchPredicate.And(x => x.FormExamine.FormMedicalID == request.Data.LoketData.FormMedicalID);
+            qry = _unitOfWork.FormExamineMedicineRepository.Get(searchPredicate, null);
+
+            foreach (var item in qry)
+            {
+                var prData = Mapper.Map<FormExamineLab, FormExamineLabModel>(item);
+
+                lists.Add(prData);
+            }
+
+            int totalRequest = lists.Count();
+            var data = lists;
+
+            var response = new FarmasiResponse
+            {
+                Draw = request.Draw,
+                RecordsFiltered = totalRequest,
+                RecordsTotal = totalRequest,
+                Data = data
+            };
+
+            return response;
+        }
+    }
+}
---
+++ Klinik.Web/Controllers/FarmasiController.cs
@@ -0,0 +1,45 @@
+﻿using Klinik.Data;
+using Klinik.Data.DataRepository;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Klinik.Web.Controllers
+{
+    public class FarmasiController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        private KlinikDBEntities _context;
+
+        #region ::DROPDOWN::
+        private List<SelectListItem> BindDropDownStatus()
+        {
+            List<SelectListItem> _status = new List<SelectListItem>();
+            _status.Insert(0, new SelectListItem
+            {
+                Text = "Open",
+                Value = "0"
+            });
+
+            _status.Insert(1, new SelectListItem
+            {
+                Text = "Waiting",
+                Value = "1"
+            });
+
+            return _status;
+        }
+
+        #endregion
+        public FarmasiController(IUnitOfWork unitOfWork, KlinikDBEntities context)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+
+        // GET: Farmasi
+        public ActionResult PatientList()
+        {
+            return View();
+        }
+    }
+}
