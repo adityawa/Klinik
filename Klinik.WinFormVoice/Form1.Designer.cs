58043f0ae848a3a6ccd8be453c85efb48b31fa1f
AWBaskoro@d-shore.nl <AWBaskoro@d-shore.nl>
2019-08-16 22:50:18 +0700

    Finish suara antrian

--- Klinik.WinFormVoice/Form1.Designer.cs
+++ Klinik.WinFormVoice/Form1.Designer.cs
@@ -28,35 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.richTxtNo = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // richTxtNo
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(166, 258);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.richTxtNo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTxtNo.Font = new System.Drawing.Font("Lucida Sans Unicode", 64F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTxtNo.Location = new System.Drawing.Point(0, 0);
+            this.richTxtNo.Name = "richTxtNo";
+            this.richTxtNo.Size = new System.Drawing.Size(488, 282);
+            this.richTxtNo.TabIndex = 0;
+            this.richTxtNo.Text = "";
+            this.richTxtNo.TextChanged += new System.EventHandler(this.RichTxtNo_TextChanged);
             // 
-            // button1
+            // timer1
             // 
-            this.button1.Location = new System.Drawing.Point(242, 82);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 69);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Voice";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(488, 282);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTxtNo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,8 +64,8 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTxtNo;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
