3d233d1fd46fd1ddb83fce59e4cbf78ca1725e18
satriax <agenjaket@gmail.com>
2019-08-18 09:04:26 +0700

    Updated lookup selection

--- Klinik.Resources/UIMessages.Designer.cs
+++ Klinik.Resources/UIMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace Klinik.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UIMessages {
@@ -2640,6 +2640,15 @@ namespace Klinik.Resources {
         public static string Sebagai {
             get {
                 return ResourceManager.GetString("Sebagai", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Select.
+        /// </summary>
+        public static string Select {
+            get {
+                return ResourceManager.GetString("Select", resourceCulture);
             }
         }
         
