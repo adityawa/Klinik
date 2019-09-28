400489c6bc4c907c24b5d4854f072e702e1da65b
arifinzainal <zainal.bangjay@gmail.com>
2019-09-29 04:14:10 +0700

    Disable few code due to missing column example Letter missing clinicId and FormExamine missing NeedSuratSakit

--- Klinik.Common/AutomapperProfile.cs
+++ Klinik.Common/AutomapperProfile.cs
@@ -274,19 +274,19 @@ namespace Klinik.Common
 
             CreateMap<Letter, LabReferenceLetterModel>();
             CreateMap<LabReferenceLetterModel, Letter>()
-                .ForMember(m => m.CreatedBy, map => map.MapFrom(p => p.Account.UserName))
-                .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID)); ;
+                .ForMember(m => m.CreatedBy, map => map.MapFrom(p => p.Account.UserName));
+                //.ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID)); ;
 
             CreateMap<SuratRujukanLabKeluar, SuratRujukanKeluarModel>();
             CreateMap<SuratRujukanKeluarModel, SuratRujukanLabKeluar>() ;
 
-            CreateMap<HealthBodyLetterModel, Letter>()
-                .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID)); ;
+            //CreateMap<HealthBodyLetterModel, Letter>()
+            //    .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID)); ;
             CreateMap<Letter, HealthBodyLetterModel>();
 
             CreateMap<RujukanBerobatModel, Letter>()
-                .ForMember(x => x.Pekerjaan, map => map.MapFrom(p => p.Perusahaan))
-                .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID));
+                .ForMember(x => x.Pekerjaan, map => map.MapFrom(p => p.Perusahaan));
+                //.ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID));
             CreateMap<Letter, RujukanBerobatModel>().ForMember(x => x.Perusahaan, map => map.MapFrom(p => p.Pekerjaan));
 
             CreateMap<PersetujuanTindakanModel, Letter>();
@@ -392,8 +392,8 @@ namespace Klinik.Common
             CreateMap<MCURegistrationInterface, MCURegistrationModel>()
                 .ForMember(x => x.strReserveDate, map => map.MapFrom(p => Convert.ToDateTime(p.RESERVE_DATE).ToString("dd/MM/yyyy")));
 
-            CreateMap<SuratIzinSakitModel, Letter>()
-                .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID));
+            //CreateMap<SuratIzinSakitModel, Letter>()
+            //    .ForMember(x => x.ClinicID, map => map.MapFrom(p => p.ClinicID));
 
 
         }
